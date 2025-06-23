fe1f6bf8c6e514701f839e082e8fbde05b161891
MOHAMED FAAHIM <146565866+Faahim2004@users.noreply.github.com>
2025-06-23 22:21:23 +0530

    Add files via upload

---
+++ Employee - Backend/Employee-Backend/src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //automatically generates id like 1,2,3
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
